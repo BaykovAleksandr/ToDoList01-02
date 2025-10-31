959fc74f5213ba67e0a007b8b8b7bab4038be831
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-31 20:27:34 +0300

    add tests for tasks-reducer

--- src/model/tasks-reducer.test.ts
+++ src/model/tasks-reducer.test.ts
@@ -1,6 +1,13 @@
 import { beforeEach, expect, test } from "vitest";
 import type { TasksState } from "../App";
-import { createTodolistAC, deleteTodolistAC, tasksReducer } from './tasks-reducer';
+import {
+  changeTaskStatusAC,
+  createTaskAC,
+  createTodolistAC,
+  deleteTaskAC,
+  deleteTodolistAC,
+  tasksReducer,
+} from "./tasks-reducer";
 
 let startState: TasksState = {};
 
@@ -41,4 +48,47 @@ test("property with todolistId should be deleted", () => {
   expect(endState["todolistId2"]).not.toBeDefined();
   // or
   expect(endState["todolistId2"]).toBeUndefined();
+});
+
+test("correct task should be deleted", () => {
+  const endState = tasksReducer(startState, deleteTaskAC("todolistId2", "2"));
+
+  expect(endState).toEqual({
+    todolistId1: [
+      { id: "1", title: "CSS", isDone: false },
+      { id: "2", title: "JS", isDone: true },
+      { id: "3", title: "React", isDone: false },
+    ],
+    todolistId2: [
+      { id: "1", title: "bread", isDone: false },
+      { id: "3", title: "tea", isDone: false },
+    ],
+  });
+});
+
+test("correct task should be created at correct array", () => {
+  const endState = tasksReducer(
+    startState,
+    createTaskAC("todolistId2", "juice")
+  );
+
+  expect(endState.todolistId1.length).toBe(3);
+  expect(endState.todolistId2.length).toBe(4);
+  expect(endState.todolistId2[0].id).toBeDefined();
+  expect(endState.todolistId2[0].title).toBe("juice");
+  expect(endState.todolistId2[0].isDone).toBe(false);
+});
+
+test("correct task should change its status", () => {
+  const endState = tasksReducer(
+    startState,
+    changeTaskStatusAC(
+      "todolistId2",
+      "2",
+      false,
+    )
+  );
+
+  expect(endState.todolistId2.length).toBe(3);
+  expect(endState.todolistId2[1].isDone).toBe(false);
 });
--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,11 +1,17 @@
-import { v1 } from 'uuid';
+import { v1 } from "uuid";
 import type { TasksState } from "../App";
 
-
-
 export type CreateTodolistAction = ReturnType<typeof createTodolistAC>;
-export type deleteTodolistAction = ReturnType<typeof deleteTodolistAC>
-type Actions = CreateTodolistAction | deleteTodolistAction;
+export type deleteTodolistAction = ReturnType<typeof deleteTodolistAC>;
+export type deleteTaskAction = ReturnType<typeof deleteTaskAC>;
+export type createTaskAction = ReturnType<typeof createTaskAC>
+export type changeTaskStatusAction = ReturnType<typeof changeTaskStatusAC>
+type Actions =
+  | CreateTodolistAction
+  | deleteTodolistAction
+  | deleteTaskAction
+  | createTaskAction
+  | changeTaskStatusAction;
 
 const initialState: TasksState = {};
 
@@ -17,17 +23,46 @@ export const tasksReducer = (
     case "create_todolist": {
       return { ...state, [action.payload.id]: [] };
     }
-    case "delete_todolist" : {
-     const newState = { ...state };
-     delete newState[action.payload.id];
-     return newState;
+    case "delete_todolist": {
+      const newState = { ...state };
+      delete newState[action.payload.id];
+      return newState;
+    }
+    case "delete_task": {
+      return {
+        ...state,
+        [action.payload.todolistId]: state[action.payload.todolistId].filter(
+          (task) => task.id !== action.payload.id
+        ),
+      };
+    }
+
+    case "create_task": {
+      const newTask = {
+        id: v1(),
+        title: action.payload.title,
+        isDone: false,
+      };
+      return {
+        ...state,
+        [action.payload.todolistId]: [
+          newTask,
+          ...state[action.payload.todolistId],
+        ],
+      };
+    }
+
+    case "change_task_status": {
+      return {
+      ...state,
+      [action.payload.todolistId]: state[action.payload.todolistId].map((task) =>
+        task.id == action.payload.id ? { ...task, isDone: action.payload.isDone } : task
+      ),
+    };
     }
     default:
       return state;
-  }
-};
-
-
+  };}
 
 export const createTodolistAC = (title: string) => {
   return { type: "create_todolist", payload: { title, id: v1() } } as const;
@@ -35,4 +70,16 @@ export const createTodolistAC = (title: string) => {
 
 export const deleteTodolistAC = (id: string) => {
   return { type: "delete_todolist", payload: { id } } as const;
-}
+};
+
+export const deleteTaskAC = (todolistId: string, id: string) => {
+  return { type: "delete_task", payload: { todolistId, id } } as const;
+};
+
+export const createTaskAC = (todolistId: string, title: string) => {
+  return {type: "create_task", payload: { todolistId, title} } as const
+};
+
+export const changeTaskStatusAC = (todolistId: string, id: string, isDone: boolean) => {
+  return {type: "change_task_status", payload: {todolistId, id, isDone}} as const
+};
