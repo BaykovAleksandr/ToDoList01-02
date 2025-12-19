f44d9657375e95a3b5b90deb99f191f515cd1c43
Baykov Aleksandr <baykov-1988@mail.ru>
2025-12-19 10:43:03 +0300

    add disable icon during deleting

--- src/features/todolists/model/todolists-slice.ts
+++ src/features/todolists/model/todolists-slice.ts
@@ -2,9 +2,11 @@ import { setAppStatusAC } from "@/app/app-slice";
 import { createAppSlice } from "@/common/utils";
 import { todolistsApi } from "../api/todolistsApi";
 import { Todolist } from "../api/todolistsApi.types";
+import { RequestStatus } from "@/common/types";
 
 export type DomainTodolist = Todolist & {
   filter: FilterValues;
+  entityStatus: RequestStatus;
 };
 export type FilterValues = "all" | "active" | "completed";
 //export const selectTodolists = (state: RootState): DomainTodolist[] => state.todolists;
@@ -31,7 +33,7 @@ export const todolistsSlice = createAppSlice({
       {
         fulfilled: (state, action) => {
           action.payload?.todolists.forEach((tl) => {
-            state.push({ ...tl, filter: "all" });
+            state.push({ ...tl, filter: "all", entityStatus: "idle" });
           });
         },
       },
@@ -55,6 +57,7 @@ export const todolistsSlice = createAppSlice({
           const newTodolist: DomainTodolist = {
             ...serverTodolist,
             filter: "all",
+            entityStatus: "idle",
           };
           state.unshift(newTodolist);
         },
@@ -64,7 +67,7 @@ export const todolistsSlice = createAppSlice({
       async (payload: { id: string }, { dispatch, rejectWithValue }) => {
         try {
           dispatch(setAppStatusAC({ status: "loading" }));
-
+          dispatch(changeTodolistStatusAC({ id: payload.id, entityStatus: "loading" }));
           await todolistsApi.deleteTodolist(payload.id);
           dispatch(setAppStatusAC({ status: "succeeded" }));
           return payload.id;
@@ -114,11 +117,26 @@ export const todolistsSlice = createAppSlice({
         todolist.filter = action.payload.filter;
       }
     }),
+    changeTodolistStatusAC: create.reducer<{
+      id: string;
+      entityStatus: RequestStatus;
+    }>((state, action) => {
+      const todolist = state.find((todolist) => todolist.id === action.payload.id);
+      if (todolist) {
+        todolist.entityStatus = action.payload.entityStatus;
+      }
+    }),
   }),
 });
 
-export const { fetchTodolistsTC, changeTodolistFilterAC, createTodolistTC, deleteTodolistTC, changeTodolistTitleTC } =
-  todolistsSlice.actions;
+export const {
+  fetchTodolistsTC,
+  changeTodolistFilterAC,
+  createTodolistTC,
+  deleteTodolistTC,
+  changeTodolistTitleTC,
+  changeTodolistStatusAC,
+} = todolistsSlice.actions;
 export const { selectTodolists } = todolistsSlice.selectors;
 
 export const todolistsReducer = todolistsSlice.reducer;
