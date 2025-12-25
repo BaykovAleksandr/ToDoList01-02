3bcada4cec9bca692524d14f079dcc7f4e64a92d
Baykov Aleksandr <baykov-1988@mail.ru>
2025-12-25 20:31:30 +0300

    add link to main page

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,13 @@
+import { Box, Button } from '@mui/material';
 import styles from "./PageNotFound.module.css";
+import { Link } from 'react-router';
 
 export const PageNotFound = () => (
-  <>
+  <Box style={{ textAlign: "center" }}>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
-  </>
+    <Button className={styles.link} component={Link} to="/" variant="contained">
+      На главную страницу
+    </Button>
+  </Box>
 );
